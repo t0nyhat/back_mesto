a059923d4097b7761c0c747bf6bcc23add9799ac
t0nyhat <elkobrat@protonmail.com>
2020-05-02 21:23:53 +1000

    add error message sending for unknown path

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const error = (req, res, next) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+  next();
+};
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/cards.js'));
 app.use('/', require('./routes/users.js'));
 
+app.use('*', error);
 app.listen(PORT, () => {});

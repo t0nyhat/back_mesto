ea62b9a4b71b0464aa014387bf5394223c92e1a2
t0nyhat <elkobrat@protonmail.com>
2020-05-02 21:16:51 +1000

    add routing for cards

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const path = require('path');
 const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/cards.js'));
+
 app.listen(PORT, () => {});

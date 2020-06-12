7ada6cd1366b57274a4cc8ae3f3a6963c7296401
t0nyhat <elkobrat@protonmail.com>
2020-06-12 11:29:29 +1000

    add to auth custom errors

--- errors/unauthorised-err.js
+++ errors/unauthorised-err.js
@@ -1,8 +1,8 @@
-class NotFoundError extends Error {
+class Unauthorised extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
   }
 }
 
-module.exports = NotFoundError;
+module.exports = Unauthorised;
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res

2c9232a809601ba872ef49c45b8024ca668ee8ae
t0nyhat <elkobrat@protonmail.com>
2020-06-12 12:05:10 +1000

    add user celebrate validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const {
   getUsers, getUsersById, patchUserInfo, patchUserAvatar,
 } = require('../controllers/user');
 
+const celebrateValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    age: Joi.number().integer().required().min(18),
+    about: Joi.string().min(2).max(30),
+  }),
+});
 
 router.get('/', getUsers);
-router.get('/:userId', getUsersById);
-router.patch('/me', patchUserInfo);
-router.patch('/me/avatar', patchUserAvatar);
+router.get('/:userId', celebrateValidation, getUsersById);
+router.patch('/me', celebrateValidation, patchUserInfo);
+router.patch('/me/avatar', celebrateValidation, patchUserAvatar);
 
 module.exports = router;

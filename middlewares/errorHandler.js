81fe464a5f4652bc8ab407ffe0b3afc512a982ea
t0nyhat <elkobrat@protonmail.com>
2020-06-12 11:06:38 +1000

    rewrite errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,23 +1,14 @@
 
-const errorHandler = (error, req, res, next) => {
-  switch (error.name) {
-    case 'CastError':
-      res.status(400).send({ message: error.message });
-      break;
-    case 'Error':
-      res.status(404).send({ message: error.message });
-      break;
-    case 'ValidationError':
-      res.status(400).send({ message: error.message });
-      break;
-    case 'Unauthorised':
-      res.status(401).send({ message: error.message });
-      break;
-    default:
-      res.status(500).send({ message: error.message });
-      break;
-  }
+const errorHandler = (err, req, res) => {
+  const { statusCode = 500, message } = err;
 
-  next();
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 };
 module.exports = { errorHandler };
